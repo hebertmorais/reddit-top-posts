7119d1fcc5322c7d90ca001f89e9ed4e36a58f25
Hebert Morais <hebertlucashllm@gmail.com>
2021-10-03 23:42:41 -0300

    test: update PostItem text props and add unread post test

--- src/components/PostItem/__tests__/RenderPostItem.tsx
+++ src/components/PostItem/__tests__/RenderPostItem.tsx
@@ -6,39 +6,52 @@ import MockPost from "./mockPost.json";
 
 describe("Assert items exist", function () {
   test("should render user name", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     const postAuthor = screen.getByText(MockPost.author);
     expect(postAuthor).toBeInTheDocument();
   });
 
   test("should render the title", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     const postTitle = screen.getByText(MockPost.title);
     expect(postTitle).toBeInTheDocument();
   });
 
   test("should render the dismiss button", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     const deleteButton = screen.getByRole("button");
     expect(deleteButton).toBeInTheDocument();
   });
 
   test("should render the number of comments", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     const commentsIndicator = screen.getByTestId("comments");
     expect(commentsIndicator).toBeInTheDocument();
   });
 
   test("should render the thumbnail", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     const postThumbnail = screen.getByRole("img");
     expect(postThumbnail).toBeInTheDocument();
   });
 
   test("should render the formatted date", function () {
-    render(<PostItem postData={MockPost} />);
+    render(<PostItem post={MockPost} />);
     Date.now = jest.fn(() => 1419819714);
     const formattedPostDate = screen.getByText("2 hours ago");
     expect(formattedPostDate).toBeInTheDocument();
+  });
+
+  test("should render the formatted date", function () {
+    render(<PostItem post={MockPost} />);
+    Date.now = jest.fn(() => 1419819714);
+    const formattedPostDate = screen.getByText("2 hours ago");
+    expect(formattedPostDate).toBeInTheDocument();
+  });
+
+  test("should render the NEW tag if the post has not been read", function () {
+    render(<PostItem post={MockPost} read={false} />);
+    const newTag = screen.getByText("NEW");
+    expect(newTag).toBeInTheDocument();
   });
 });
