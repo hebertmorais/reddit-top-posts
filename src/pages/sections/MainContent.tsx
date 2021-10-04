94a722257516d601b5e2426ff88a39f76a9545cb
Hebert Morais <hebertlucashllm@gmail.com>
2021-10-04 08:49:43 -0300

    feat: add dismiss all itens button

--- src/pages/sections/MainContent.tsx
+++ src/pages/sections/MainContent.tsx
@@ -1,37 +1,60 @@
-import React, { useEffect } from "react";
-import { Flex, Box } from "@chakra-ui/react";
+import React from "react";
+import { Flex, Box, Button } from "@chakra-ui/react";
 import ContentViewer from "./ContentViewer";
 import PostsList from "./PostsList";
-import { postsSelector, selectPost } from "../../redux/postsSlice";
+import {
+  postsSelector,
+  selectPost,
+  dismissAllPosts,
+} from "../../redux/postsSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { SmallCloseIcon } from "@chakra-ui/icons";
 
 function MainContent() {
-  const { selectedPost } = useSelector(postsSelector);
+  const { posts, selectedPost } = useSelector(postsSelector);
   const dispatch = useDispatch();
 
   return (
-    <Flex width={{ base: "100%", lg: "1200px" }} height="100%">
-      <Box width={{ base: "100%", lg: "65%" }} margin={4} padding={4}>
-        <PostsList />
-      </Box>
-
-      {selectedPost && (
-        <Box
-          borderRadius={8}
-          width={{ base: "full", lg: "35%" }}
-          height={{ base: "full", lg: "initial" }}
-          position={{ base: "absolute", lg: "initial" }}
-          margin={4}
-        >
-          <ContentViewer
-            post={selectedPost}
-            onClose={() => {
-              dispatch(selectPost(null));
-            }}
-          />
+    <>
+      <Flex width={{ base: "100%", lg: "1200px" }} height="100%">
+        <Box width={{ base: "100%", lg: "65%" }} margin={4} padding={4}>
+          <Flex flex="1">
+            {posts.length > 0 && (
+              <Button
+                leftIcon={<SmallCloseIcon />}
+                colorScheme="orange"
+                variant="outline"
+                size="small"
+                padding={1}
+                margin={1}
+                borderRadius="full"
+                onClick={() => dispatch(dismissAllPosts(null))}
+              >
+                Dismiss all
+              </Button>
+            )}
+          </Flex>
+          <PostsList />
         </Box>
-      )}
-    </Flex>
+
+        {selectedPost && (
+          <Box
+            borderRadius={8}
+            width={{ base: "full", lg: "35%" }}
+            height={{ base: "full", lg: "initial" }}
+            position={{ base: "absolute", lg: "initial" }}
+            margin={4}
+          >
+            <ContentViewer
+              post={selectedPost}
+              onClose={() => {
+                dispatch(selectPost(null));
+              }}
+            />
+          </Box>
+        )}
+      </Flex>
+    </>
   );
 }
 
